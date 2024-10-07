a8c8945ea3c69827140621b174705e27d162906d
h0undgr1n <173692627+h0undgr1n@users.noreply.github.com>
2024-10-07 14:28:13 +0100

    complete 02 and 03, start 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(listArr, ...otherArgs) {
+    for (let element in otherArgs){
+        if (otherArgs[element] in listArr){
+            const index = listArr.indexOf(otherArgs[element]);
+            const x = listArr.splice(index, 1);
+            return(listArr)
+        }
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
 
 // Take notes here first:
+// It may be the case that I can assign the arr and the other value to variables 
