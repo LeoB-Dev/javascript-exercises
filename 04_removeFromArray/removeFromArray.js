ef1139109b371cddff4e6897717ecab9e10d16bd
h0undgr1n <173692627+h0undgr1n@users.noreply.github.com>
2024-10-14 14:59:55 +0100

    Complete exercise 5, exercise 4 4/7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,15 @@
 const removeFromArray = function(listArr, ...otherArgs) {
     for (let element in otherArgs){
-        while (otherArgs[element] in listArr){
-            const index = listArr.indexOf(otherArgs[element]);
-            const x = listArr.splice(index, 1);
+        if (!(otherArgs[element] in listArr)){
+            continue;
+        } else if (otherArgs[element] in listArr){
+                const index = listArr.indexOf(otherArgs[element]);
+                const x = listArr.splice(index, 1);
         }
     }
     return(listArr)
-};
+}
+
 
 
 
@@ -14,4 +17,4 @@ const removeFromArray = function(listArr, ...otherArgs) {
 module.exports = removeFromArray;
 
 // Take notes here first:
-// It may be the case that I can assign the arr and the other value to variables 
+// See mdn loops
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,39 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let numList = [];
+    let sum = 0;
 
+    if (num1 >= 0 
+        && num2 >= 0 
+        && typeof num1 === 'number' 
+        && typeof num2 === 'number'
+        && (num1 % 1) == 0
+        && (num2 % 1) == 0){
+        if (num1 < num2){
+            numList.push(num1);
+            sum += num1;
+    
+            while (num1 < num2){
+                num1++
+                numList.push(num1);
+                sum+=num1;
+            }
+        } else {
+            numList.push(num1);
+            sum+= num1;
+    
+            while(num2 < num1){
+                num1--
+                numList.push(num1);
+                sum+=num1;
+            }
+        }
+    } else {
+        return("ERROR")
+    }
+    // console.log("Sum:", sum);
+    return(sum);
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
